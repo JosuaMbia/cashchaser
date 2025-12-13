1e849b64ce7b018905d236c5949c7d426429f323
JosuaMbia <josuambia75@gmail.com>
2025-12-13 13:46:50 +0000

    fix: Correct syntax error in session?.user check

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -57,7 +57,7 @@ export const authOptions: NextAuthOptions = {
       return token
     },
     async session({ session, token }) {
-      if (s?ssion.user) {
+          if (session?.user) {
         session.user.id = token.id as string
         session.user.companyId = token.companyId as string
       }
